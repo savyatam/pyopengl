347e1b54c187a050dabae3b6ae25e536c14e68a0
savyatam <shobha@myhomejam.com>
2021-08-25 01:43:36 +0530

    initial commit

---
+++ pyg.py
@@ -0,0 +1,264 @@
+import pygame
+import sys
+from OpenGL.GL import *
+from OpenGL.GLU import *
+import cv2
+
+vertices = (
+    # x  y  z
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+)
+
+edges = (
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7)
+)
+
+
+def loadTexture1():
+    textureSurface = pygame.image.load('1.jpeg')
+    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    width = textureSurface.get_width()
+    height = textureSurface.get_height()
+
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+    return texid
+
+def loadTexture2():
+    textureSurface = pygame.image.load('2.jpeg')
+    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    width = textureSurface.get_width()
+    height = textureSurface.get_height()
+
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+    return texid
+
+def loadTexture3():
+    textureSurface = pygame.image.load('index.jpeg')
+    textureData = pygame.image.tostring(textureSurface, "RGBA", 1)
+    width = textureSurface.get_width()
+    height = textureSurface.get_height()
+
+    glEnable(GL_TEXTURE_2D)
+    texid = glGenTextures(1)
+
+    glBindTexture(GL_TEXTURE_2D, texid)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height,
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData)
+
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+
+    return texid
+
+
+def draw_cube1(lines=False):
+    
+    if lines:
+        glBegin(GL_LINES)
+        for edge in edges:
+            glColor3fv((1, 1, 1))
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+    else:
+        glBegin(GL_QUADS)
+
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0, -1.0)
+        
+
+
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+
+
+
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0, -1.0,  1.0)
+
+
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(-1.0, -1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0, -1.0, -1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0, -1.0,  -1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0,  -1.0)
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+
+       
+
+
+        glEnd()
+
+
+def draw_cube3(lines=False):
+    
+    if lines:
+        glBegin(GL_LINES)
+        for edge in edges:
+            glColor3fv((1, 1, 1))
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+    else:
+        glBegin(GL_QUADS)
+
+
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  1.0, -1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(-1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(1.0,  1.0, -1.0)
+        glEnd()
+
+
+
+
+def draw_cube2(lines=False):
+    
+    if lines:
+        glBegin(GL_LINES)
+        for edge in edges:
+            glColor3fv((1, 1, 1))
+            for vertex in edge:
+                glVertex3fv(vertices[vertex])
+        glEnd()
+    else:
+        glBegin(GL_QUADS)
+
+
+        glTexCoord2f(0.0, 1.0)
+        glVertex3f(-1.0,  -1.0, 1.0)
+        glTexCoord2f(0.0, 0.0)
+        glVertex3f(1.0,  -1.0,  1.0)
+        glTexCoord2f(1.0, 0.0)
+        glVertex3f(1.0,  1.0,  1.0)
+        glTexCoord2f(1.0, 1.0)
+        glVertex3f(-1.0,  1.0, 1.0)
+
+ 
+
+
+        glEnd()
+
+    
+
+pygame.init()
+display = (800, 600)
+screen = pygame.display.set_mode(
+    display, pygame.DOUBLEBUF | pygame.OPENGL | pygame.OPENGLBLIT)
+
+
+
+gluPerspective(45, display[0] / display[1], 0.1, 50.0)
+glTranslatef(0.0, 0.0, -10)
+i=0
+imagev = cv2.imread("1.jpeg")
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYUP:
+            i+=1
+            filename = "fol/"+str(i)+".jpeg"
+            size = screen.get_size()
+            buffer = glReadPixels(0, 0, *size, GL_RGBA, GL_UNSIGNED_BYTE)
+            screen_surf = pygame.image.fromstring(buffer, size, "RGBA")
+            view = pygame.surfarray.array3d(screen_surf)
+            view = view.transpose([1, 0, 2])
+            img_bgr = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
+            cv2.imwrite(filename, img_bgr)
+            #pygame.image.save(screen_surf,filename)
+            
+            
+    glRotatef(1, 1, 3, 1)
+    glEnable(GL_DEPTH_TEST);
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    loadTexture1()
+    draw_cube1(lines=False)
+    loadTexture2()
+    draw_cube2(lines=False)
+    
+    #loadTexture3()
+    #draw_cube3(lines=False)
+    
+    pygame.display.flip()
+
+    pygame.time.wait(10)
+    
+    
+
